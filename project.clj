7dce1de198fa376b611834ff8ae2b62a1a780b5d
gered <gered@blarg.ca>
2022-01-30 12:44:54 -0500

    bump to next snapshot version for future development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.gered/lein-template.simple-app "0.2.0"
+(defproject net.gered/lein-template.simple-app "0.2.1-SNAPSHOT"
   :description         "Simple Clojure CLI app or tool project template."
   :url                 "https://github.com/gered/simple-app-template"
   :license             {:name "MIT License"
